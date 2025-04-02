9c7c6faf6f2e424675dfd10630107b0c86c30ae6
joaovzatta <joao.zatta@escola.pr.gov.br>
2025-04-02 13:44:48 +0000

    iiii

--- script.js
+++ script.js
@@ -19,3 +19,8 @@ document.addEventListener('DOMContentLoaded', function(){
 
 const botaoDeAcessibilidade = document.getElementById('botao-acessiblidade')
 const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+ botaoDeAcessibilidade.addEventListener('click', function(){
+    botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+    opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+ })
+ 
